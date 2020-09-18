c1e80793bb33cd96abe8c364fc7fae9adb099e5b
jesuskata <rzarate.alex@gmail.com>
2020-09-18 01:06:26 -0500

    feat(api): 🎸 integrate audioboom api

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,61 @@
-/* eslint-disable react/jsx-one-expression-per-line */
 // Dependencies
 import React from 'react';
+import PropTypes from 'prop-types';
+import 'isomorphic-fetch';
 
-const Index = () => (
+const Home = ({ channels }) => (
   <div className="container">
-    <h1>Home Page</h1>
-    <p>Bulbasaur is here</p>
-    <img src="/001.gif" alt="pokemon-001" />
+    <header>Podcast</header>
+    <div className="channels">
+      {channels.map(channel => (
+        <div className="channel">
+          <img src={channel.urls.logo_image.original} alt={channel.title} />
+          <h2>{channel.title}</h2>
+        </div>
+      ))}
+    </div>
     <style jsx>
       {`
+        header {
+          color: #333333;
+          background: #A2F6A6;
+          padding: 15px;
+          text-align: center;
+          font-weight: 700;
+        }
+
         .container {
           background-color: #20313C;
           color: white;
           font-family: 'Roboto';
-        }
-        div {
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          text-align: center;
-          height: 100vh;
-          margin: auto 0;
+          justify-content: space-between;
         }
-        img {
+
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+
+        .channel {
           display: block;
-          margin: 0 auto;
+          margin-bottom: 0.5em;
+        }
+
+        .channel img {
+          border-radius: 3px;
+          width: 100%;
+        }
+
+        .channel h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
         }
       `}
     </style>
@@ -38,4 +69,14 @@ const Index = () => (
   </div>
 );
 
-export default Index;
+Home.propTypes = {
+  channels: PropTypes.arrayOf(PropTypes.object)
+};
+
+export async function getServerSideProps() {
+  const req = await fetch('https://api.audioboom.com/channels/recommended');
+  const { body: channels } = await req.json();
+  return { props: { channels } };
+}
+
+export default Home;
